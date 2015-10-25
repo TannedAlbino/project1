b8492dafcde06df67e5ec7d37bba21f4040cd9a0
Uriel <uriel.chavez@gmail.com>
2015-10-25 11:25:51 -0700

    limit to 10 gifs

--- public/app.js
+++ public/app.js
@@ -11,7 +11,7 @@ function App(){};
 
 
 App.prototype.getMotivation = function(){
-	var url = "https://api.giphy.com/v1/gifs/search?limit=10,q=Motivation&api_key=dc6zaTOxFJmzC"
+	var url = "https://api.giphy.com/v1/gifs/search?q=Motivation&limit=10&api_key=dc6zaTOxFJmzC"
 	var that = this
 	$.ajax({
 		// this is a GET request. I'm keeping it explicate for you, 
@@ -29,7 +29,7 @@ App.prototype.getMotivation = function(){
 	});
 };
 App.prototype.getFunny = function(){
-	var url = "https://api.giphy.com/v1/gifs/search?limit=10,q=funniest&api_key=dc6zaTOxFJmzC"
+	var url = "https://api.giphy.com/v1/gifs/search?limit=10&q=funniest&api_key=dc6zaTOxFJmzC"
 	var that = this
 	$.ajax({
 		// this is a GET request. I'm keeping it explicate for you, 
